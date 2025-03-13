8f1cf53456840252f347e5632ab11b9eb070c7ac
william-campos-sys <williamcampos1985@gmail.com>
2025-03-13 20:36:03 -0300

    Create ex-05.py

---
+++ ex-05.py
@@ -0,0 +1,7 @@
+Você foi contratado por uma escola pra fazer o sistema de boletim dos alunos. Como primeiro passo, escreva um programa que produza a seguinte saída:
+print("ALUNO(A)          NOTA")
+print("=========         =====")
+print("ALINE              9.0")
+print("MÁRIO              DEZ")
+print("SÉRGIO             4.5")   
+print("SHIRLEY            7.0")
