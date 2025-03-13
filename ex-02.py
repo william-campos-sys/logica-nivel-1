b9e0fbabfe543a40ac872a51bdffea6b6f5c0917
william-campos-sys <williamcampos1985@gmail.com>
2025-03-13 20:13:37 -0300

    Create ex-02.py

---
+++ ex-02.py
@@ -0,0 +1,4 @@
+# Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+print("ana beatriz")
+print("rio de janeiro")
+print("duque de caxias")
