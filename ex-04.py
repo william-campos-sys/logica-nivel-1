7d45feeee0a518a5c02383fe0d970b797d1825d8
william-campos-sys <williamcampos1985@gmail.com>
2025-03-13 20:24:30 -0300

    Create ex-04.py

---
+++ ex-04.py
@@ -0,0 +1,12 @@
+# Escrever um programa que imprima a seguinte figura:
+
+# XXXXX
+# X   X
+# X   X
+# X   X
+# XXXXX
+print ("XXXXX")
+print("X   X")
+print("X   X")
+print("X   X")
+print ("XXXXX")
