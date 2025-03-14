3da2195caa37e70af1042a255967fc19d55b1459
william-campos-sys <williamcampos1985@gmail.com>
2025-03-13 21:41:19 -0300

    Update ex-03.py

--- ex-03.py
+++ ex-03.py
@@ -1,2 +1,2 @@
- Faça um programa que imprima na tela o nome de uma música que você gosta (proibido letras do Justin Bieber).
+#Faça um programa que imprima na tela o nome de uma música que você gosta (proibido letras do Justin Bieber).
 print("brasileirinha")
